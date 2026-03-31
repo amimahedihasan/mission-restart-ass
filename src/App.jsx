bac7605eb8870a642a785e405e90dce6eb509836
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-03-31 19:17:52 +0600

    Implement Home page layout and banner section

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
